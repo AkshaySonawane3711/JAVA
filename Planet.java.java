24a414e049fb36cd0d79d76e261a9d142a17ea2b
AKSHAY SONAWANE <77873120+AkshaySonawane3711@users.noreply.github.com>
2021-02-09 21:59:49 +0530

    Add files via upload

---
+++ Planet.java.java
@@ -0,0 +1,9 @@
+class planet {
+        void revolve() {
+        System.out.println("REVOLVE");
+   }
+     public static void main(string[] args) {
+       Planet earth = new Planet();
+       earth.revolve();
+   }
+   }
